f1605846e9ee2644b359137810d92aa5de2e0341
jenifergs <contato.comercialjenifer@gmail.com>
2022-10-14 21:16:48 -0300

    feat: :white_check_mark: Exercicios finalizados 9.1

---
+++ secao-8/dia-1/exercicio-dia-1/Person.ts
@@ -0,0 +1,9 @@
+class Person {
+    name: string;
+    birthDate: Date;
+
+    constructor(name: string, birthDate: Date) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+};
--- secao-8/dia-1/exercicio-dia-1/index.ts
+++ secao-8/dia-1/exercicio-dia-1/index.ts
@@ -3,7 +3,7 @@ import Students from './Students';
 const student1 = new Students("Rafael", 100) 
 
 student1.provas = [] 
-student1.trabalhos = [3,4]
+student1.traba.prettierrclhos = [3,4]
 
 console.log(student1.mediaDasNotas());
 console.log(student1.somaDasNotas());
