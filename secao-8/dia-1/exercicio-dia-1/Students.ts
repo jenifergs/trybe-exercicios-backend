b255ccda8902b521f78a0d1c6e4a3a80362248cf
jenifergs <contato.comercialjenifer@gmail.com>
2022-10-16 14:54:03 -0300

    feat: :white_check_mark: Adiciona exercicios 9.2

--- secao-8/dia-1/exercicio-dia-1/Students.ts
+++ secao-8/dia-1/exercicio-dia-1/Students.ts
@@ -1,18 +1,17 @@
-export default class Students {
-  private _nome: string;
-  private _matricula: number;
+import Person from "./Person";
+import { v4 as uuidv4 } from 'uuid';
+
+export default class Students extends Person {
+  private _matricula: number | string;
   _provas: number[];
   _trabalhos: number[];
 
-  constructor(name: string, matricula: number) {
-    this._nome = name;
-    this._matricula = matricula;
+  constructor(name: string, birthDate: Date) {
+    super(name, birthDate );
+    // this._nome = name;
+    this._matricula = this.generateEnrollment();
     this._provas = [];
     this._trabalhos = [];
-  }
-
-  get nome() {
-    return this._nome;
   }
 
   get matricula() {
@@ -45,6 +44,11 @@ export default class Students {
     const soma = this.somaDasNotas();
     const media = this.somaDasNotas() / this._provas.length;
     return media;
+  }
+
+  generateEnrollment(): string {
+    const random = uuidv4().slice(0, 16);
+    return random;
   }
 
 }
--- secao-8/dia-1/exercicio-dia-1/index.ts
+++ secao-8/dia-1/exercicio-dia-1/index.ts
@@ -1,9 +1,29 @@
 import Students from './Students';
+import Person from './Person';
 
-const student1 = new Students("Rafael", 100) 
+// const student1 = new Students("Rafael", 100) 
 
-student1.provas = [] 
-student1.traba.prettierrclhos = [3,4]
+// student1.provas = [] 
+// // student1.traba.prettierrclhos = [3,4]
 
-console.log(student1.mediaDasNotas());
-console.log(student1.somaDasNotas());
+// console.log(student1.mediaDasNotas());
+// console.log(student1.somaDasNotas());
+
+
+const jenifer = new Person('Jenifer', new Date('1990-01-01'));
+console.log(jenifer);
+console.log(jenifer.name);
+console.log(jenifer.birthDate);
+const rafael = new Person('Rafael', new Date('2000-11-29'));
+
+console.log(rafael);
+console.log(rafael.name);
+console.log(rafael.birthDate);
+
+const novo = new Students('jeje', new Date('2000-11-17'));
+console.log(novo);
+
+const eu1 = new Students('Rafael', new Date('2000-11-29'));
+console.log(eu1);
+console.log(eu1.matricula);
+
